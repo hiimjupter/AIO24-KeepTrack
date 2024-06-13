81104385657aa0eb977bba65dc16733fe3ddc449
JupterMaccy <trunghieu.nguyen1107@gmail.com>
2024-06-13 23:01:26 +0700

    feat: Update test case for SonarCloud coverage

--- Week2/compare_value.py
+++ Week2/compare_value.py
@@ -2,5 +2,5 @@ def compare_value(num_list, n):
     return any(num == n for num in num_list)
 
 
-assert compare_value([1, 3, 9, 4], -1) == False
+assert compare_value([1, 3, 9, 4], 9) == True
 print(compare_value([1, 2, 3, 4], 2))
